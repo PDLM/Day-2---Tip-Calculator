a1bf439f73eb9b49df5a139e9be53a430acf54ce
Randall Dennison <r.dennison7@gmail.com>
2023-06-25 19:46:05 -0400

    Day 2 of 100 Days of Python

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator.")
+bill_total = int(input("What was the total bill? $"))
+tip_input = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+num_people = int(input("How many people to split the bill? "))
+
+tip_percent = (float(tip_input) * .01) + 1
+bill_plus_tip = bill_total * tip_percent
+bill_per_person = round(bill_plus_tip / num_people, 2)
+print(f"Each person should pay: ${bill_per_person}")
